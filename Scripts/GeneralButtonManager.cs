c70bc920fe1711d9a7ccea1ff9e758823ab735be
DemirK2 <dkutluoglu@gmail.com>
2020-11-12 00:21:31 +0300

    Removed "Manager.cs".
    Removed "Manager.cs" and added "GeneralButtonManager.cs", it has everything the old file had.

---
+++ Scripts/GeneralButtonManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GeneralButtonManager : MonoBehaviour {
+
+    /*
+    This script contains a lot o useful stuff that can be used via buttons.
+    This script gets updated often, check its GitHub Repository for newer versions.
+    https://github.com/DemirK2/Unity
+    
+    Support:
+    https://discord.gg/STcThtu
+    https://www.pokenix.com
+    */
+
+    // Loads a scene.
+    public void SceneLoader(int SceneID)
+    {
+        SceneManager.LoadScene(SceneID);
+    }
+
+    // Opens an URL (default browser).
+    public void OpenURL(string URL)
+    {
+        Application.OpenURL(URL);
+    }
+
+    // Closes the application down.
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    // Disables the cursor.
+    public void DisableCursor()
+    {
+        Cursor.visible = false;
+        Cursor.LockState = CursorLockMode.Locked;
+    }
+
+    // Enables the cursor.
+    public void EnableCursor()
+    {
+        Cursor.visible = true;
+        Cursor.LockState = CursorLockMode.None;
+    }
+}
